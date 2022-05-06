9c5e928c3afc7e35fcfb605facbe0446c8584500
glawkdxn0328 <glawkdxn0328@naver.com>
2022-05-06 11:37:56 +0900

    keys

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# terra_dapp_1
+# terra_dapp_1<br>
+ref : https://quiet-lilac-e5d.notion.site/3ff449f9252a4cc4831941a67118b039
--- my_terra_dapp/keys.terrain.js
+++ my_terra_dapp/keys.terrain.js
@@ -4,9 +4,9 @@
 module.exports = {
   custom_tester_1: {
     mnemonic:
-      "shiver position copy catalog upset verify cheap library enjoy extend second peasant basic kit polar business document shrug pass chuckle lottery blind ecology stand",
+      "cave stock fine tone gauge cycle cotton flavor wrap mule drama secret admit mixture suit swing story breeze gadget number square armed away life,
   },
   custom_tester_2: {
-    privateKey: "fGl1yNoUnnNUqTUXXhxH9vJU0htlz9lWwBt3fQw+ixw=",
+    privateKey: "eyJuYW1lIjoiZGRkZCIsImFkZHJlc3MiOiJ0ZXJyYTFsd3I1Zmt0cm1ja3lndDJzeWd1aDJzdTducXVteTIyODJmenZoOCIsImVuY3J5cHRlZF9rZXkiOiI2NDZlZTlkZDcwZTcyOGRhYTYzMTVjMzVlNmM3ZDgyZDE4NDgwMjBjZjE5MjcyZDg4MmE5N2NmMGY4NjI0YjM3U2RMdEppTkU2MUxVTWZvaUpTTGhyUEl4aTE2VkdZQWNqUzlEbjg3WnZVTHNpQUtRS2RCU0xnRWliTW03YWp1L29XcWlXZzB1RVBManFkSFZ4eUc3ZDRWc3dzbExQQ21vYzFkbGg5SnRreEk9In0=",
   },
 };
